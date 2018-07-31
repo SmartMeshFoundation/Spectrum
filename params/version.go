1023ddc9b9942bd49528bc9d00bf04b0b786023e
liangchuan <cc14514@icloud.com>
2018-07-31 17:30:03 +0800

    rc-0.3.3

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 0       // Major version component of the current release
 	VersionMinor = 3       // Minor version component of the current release
 	VersionPatch = 3       // Patch version component of the current release
-	VersionMeta  = "dev"
+	VersionMeta  = "release"
 	//VersionMeta  = "release" // alpha / beta / release / stable
 )
 
