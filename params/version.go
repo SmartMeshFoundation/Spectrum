f085b3694533451fd3ea05934adba853a711af7b
cc14514 <cc14514@icloud.com>
2018-09-14 12:31:46 +0800

    Update version.go

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 0 // Major version component of the current release
 	VersionMinor = 5 // Minor version component of the current release
 	VersionPatch = 1 // Patch version component of the current release
-	VersionMeta  = "unstable"
+	VersionMeta  = "release"
 	//VersionMeta  = "release" // alpha / beta / release / stable
 )
 
