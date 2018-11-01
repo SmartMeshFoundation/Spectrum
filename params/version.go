8ac6983566effd85f31adc27dc07776c987ad02e
liangchuan <cc14514@icloud.com>
2018-11-01 16:50:06 +0800

    pre upgrade

--- params/version.go
+++ params/version.go
@@ -23,8 +23,8 @@ import (
 const (
 	VersionMajor = 0 // Major version component of the current release
 	VersionMinor = 5 // Minor version component of the current release
-	VersionPatch = 1 // Patch version component of the current release
-	VersionMeta  = "release"
+	VersionPatch = 2 // Patch version component of the current release
+	VersionMeta  = "unstable"
 	//VersionMeta  = "release" // alpha / beta / release / stable
 )
 
