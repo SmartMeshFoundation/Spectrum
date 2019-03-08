377eaa45325be06b9b68c4a10e4d049b240c6724
liangchuan <cc14514@icloud.com>
2019-03-08 15:03:17 +0800

    vsn 0.6.x

--- params/version.go
+++ params/version.go
@@ -22,9 +22,9 @@ import (
 
 const (
 	VersionMajor = 0 // Major version component of the current release
-	VersionMinor = 5 // Minor version component of the current release
-	VersionPatch = 3 // Patch version component of the current release
-	VersionMeta  = "rc"
+	VersionMinor = 6 // Minor version component of the current release
+	VersionPatch = 0 // Patch version component of the current release
+	VersionMeta  = "dev"
 )
 
 // Version holds the textual version string.
