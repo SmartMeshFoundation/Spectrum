f87f288a9b88945a93551fbd1fd94217cc60a178
bai <nkbai@163.com>
2019-07-10 15:12:45 +0800

    contracts: add a demo chiefBaseOwer contract.

---
+++ contracts/chief/src/chiefbaseowner_alpha.sol
@@ -0,0 +1,21 @@
+contract ChiefBaseInterface {
+    function pocChangeOwner(address newOwner, uint256 num)   public;
+
+}
+/*
+该合约有两个主要功能:
+1. 依据规则增减常委会节点
+2. 辅助下一个版本硬分叉升级Chief合约,到时候我们希望只升级Chief,以及ChiefBase合约,但是不升级poc合约.
+*/
+contract ChiefBaseOwner {
+    ChiefBaseInterface base;
+    constructor(address chiefBase) public {
+        base=chiefBase;
+    }
+    function UpgradeToNewChiefVersion( address newChiefBase,uint256 num) public{
+        //msg.sender保证不存在,这样在硬分叉的指定块强制调用这个合约. 
+        require(msg.sender==address(0xffffffffffffffffffffffffffffffffffffffff));
+        base.pocChangeOwner(newChiefBase,num);
+        base=newChiefBase;
+    }
+}
